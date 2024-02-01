cc44bb430c58714b2166767d12f1a749ccd43cfd
ninjasploit <ninjasploit@users.noreply.github.com>
2024-02-01 20:39:39 +0100

    Set up skeleton of auth router

---
+++ src/routes/auth/index.ts
@@ -0,0 +1,13 @@
+// Import
+import { Router } from "express";
+
+// Create router
+const router = Router();
+
+// TODO: Add basic ratelimiting?
+
+// TODO: Create register endpoint
+
+// TODO: Create login endpoint
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,16 @@
+// Import
 import { Router } from "express";
+import AuthRouter from "./auth";
 
+// Create router
 const router = Router();
 
+// Hello, world endpoint
 router.get("/", (req, res) => {
   res.end("Hello, world!");
 });
+
+// Use our auth router
+router.use("/auth", AuthRouter);
 
 export default router;
