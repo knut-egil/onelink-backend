715e45cfdbab36a854e5565c8848e3ea97ef0754
ninjasploit <ninjasploit@users.noreply.github.com>
2024-02-01 19:18:13 +0100

    Friendly response :)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 // Import
 import express from "express";
+import routes from "./routes";
 
 // Set up express app
 const app = express();
 
 // Set up middleware
 app.use(express.json());
+
+// Use routes!
+app.use(routes);
 
 // Define listen port
 const PORT = process.env.PORT ?? 3000;
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.end("Hello, world!");
+});
+
+export default router;
